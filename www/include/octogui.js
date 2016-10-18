248906601de65dabc56f81600a1e3afb26a0a951
Ryan <ryan@seemecnc.com>
2016-10-18 12:51:37 -0400

    Extrude 2mm of filament on resumeHotLoad

--- www/include/octogui.js
+++ www/include/octogui.js
@@ -597,7 +597,7 @@ function connectPrinter(com){
 function resumeHotLoad(){
   document.getElementById('hotUnload').style.visibility = "hidden";
   if(hotLoading){
-    sendCommand( ["90", "G0 Z" + returnZ +" F1440", "G92 E" + returnE ] );
+    sendCommand( ["90", "G0 Z" + returnZ +" F1440 E2", "G92 E" + returnE ] );
     document.getElementById('hotLoad').style.visibility = "hidden";
     hotLoading = false;
     returnE = 0;
