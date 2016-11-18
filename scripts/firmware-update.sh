64c3bc72b3c2a1d6617d1bb3d539c3e2b483aaa5
Ryan <ryan@seemecnc.com>
2016-11-18 10:19:28 -0500

    firmware-update finds the correct port before every command

--- scripts/firmware-update.sh
+++ scripts/firmware-update.sh
@@ -5,6 +5,23 @@ then
   exit 1
 fi
 
+function getPort {
+  let p=0
+  while [ $p -lt 9 -a -z "$port" ]
+  do
+    if [ -e /dev/ttyACM$p ]
+    then
+      port=/dev/ttyACM$p
+    fi
+  done
+  if [ -n "$port" ]
+  then
+    echo $port
+  else
+    exit 1
+  fi
+}
+
 problem="null"
 eclear="/var/www/html/OctoGUI/scripts/eeprom_clear.hex"
 
@@ -34,7 +51,7 @@ then
   sudo /usr/local/bin/hub-ctrl -h 0 -P 2 -p 1
   echo "DONE"
   sleep 2
-  avrdude -v -p m2560 -c stk500v2 -P /dev/ttyACM0 -b 115200 -D -U flash:w:$eclear:i
+  avrdude -v -p m2560 -c stk500v2 -P $(getPort) -b 115200 -D -U flash:w:$eclear:i
   sleep 2
   echo -n "Cycling USB bus ... "
   sudo /usr/local/bin/hub-ctrl -h 0 -P 2 -p 0
@@ -42,9 +59,9 @@ then
   sudo /usr/local/bin/hub-ctrl -h 0 -P 2 -p 1
   echo "DONE"
   sleep 2
-  avrdude -v -p m2560 -c stk500v2 -P /dev/ttyACM0 -b 115200 -D -U flash:w:$1:i
+  avrdude -v -p m2560 -c stk500v2 -P $(getPort) -b 115200 -D -U flash:w:$1:i
   sleep 15
-  curl -H "Content-Type: application/json" -X POST -d '{"command":"connect","port":"/dev/ttyACM0"}' http://localhost/api/connection?apikey=ABAABABB
+  curl -H "Content-Type: application/json" -X POST -d "{\"command\":\"connect\",\"port\":\"$(getPort)\"}" http://localhost/api/connection?apikey=ABAABABB
 else
   echo "Cannot flash firmware when printer is $problem"
 fi
