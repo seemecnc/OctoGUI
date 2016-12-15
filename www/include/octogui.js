c3b70ccc9d43c3d2b98c0b6c2d2a8b987e5bfe46
Ryan Rittenhouse <ryan@redpost.co>
2016-12-14 21:18:24 -0500

    prototype push of all needed eeprom values

--- www/include/octogui.js
+++ www/include/octogui.js
@@ -283,11 +283,46 @@ function spottedLog(key, log){
       switch(t+bits[1]){
 
         case "1893": // X endstop
-          console.log("206 T1 P893 S" + bits[2]);
+          EEProm.push("206 T1 P893 S" + bits[2]);
           break;
 
+        case "1895": // Y endstop
+          EEProm.push("206 T1 P895 S" + bits[2]);
+          break;
+
+        case "1897": // X endstop
+          EEProm.push("206 T1 P897 S" + bits[2]);
+          break;
+
+        case "3901": // Alpha A
+          EEProm.push("206 T3 P901 X" + bits[2]);
+          break;
+
+        case "3905": // Alpha B
+          EEProm.push("206 T3 P905 X" + bits[2]);
+          break;
+
+        case "3909": // Alpha C
+          EEProm.push("206 T3 P909 X" + bits[2]);
+          break;
+
+        case "3881": // Diagonal Rod Length
+          EEProm.push("206 T3 P881 X" + bits[2]);
+          break;
+
+        case "3885": // Horizontal Radius
+          EEProm.push("206 T3 P885 X" + bits[2]);
+          break;
+
+        case "3153": // Z Max Length
+          EEProm.push("206 T3 P153 X" + bits[2]);
+          break;
+
+
         case "3246": // Last EEProm value
-          console.log("DONE");
+          EEProm.push("M500");
+          EEProm.push("M117 Calibration Restored");
+          console.log(EEProm);
           delete watchLogFor[key]; watchLogFor.length--;
           break;
 
@@ -298,7 +333,6 @@ function spottedLog(key, log){
 
 function backupCalibration(){
 
-  console.log("Backup up eeprom");
   EEProm = [];
   watchLogFor["backupEEProm"] = "Recv: EPR:"; watchLogFor.length++;
   sendCommand("M205");
