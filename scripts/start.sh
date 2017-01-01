de51028bbb0dbf956a77ad0018fc6c6b0bb56177
Ryan Rittenhouse <ryan@redpost.co>
2017-01-01 13:37:04 -0500

    Added NOGUI option to avoid loading the GUI at login/boot

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,45 +1,58 @@
 #!/bin/bash
-if [ -f /home/pi/chromium.tar.gz -a ! -d /tmp/chromium ]
+
+if [ -f /home/pi/NOGUI ]
 then
-  rm -rf /home/pi/.config/chromium
-  tar -xzf /home/pi/chromium.tar.gz -C /tmp
-  ln -nsf /tmp/chromium /home/pi/.config/chromium
-fi
-DISPLAY=:0.0 /usr/bin/xset s off
-DISPLAY=:0.0 /usr/bin/xset -dpms
-DISPLAY=:0.0 /usr/bin/xset s noblank
-DISPLAY=:0.0 /usr/bin/unclutter -idle 0.1 &
-if [ -f /home/pi/BURNIN ]
-then
-  DISPLAY=:0.0 /usr/bin/chromium-browser --kiosk http://localhost/seeme/OctoGUI/www/burnin.html --fullscreen 2> /dev/null &
-else
-  DISPLAY=:0.0 /usr/bin/chromium-browser --kiosk http://localhost/seeme/ --fullscreen 2> /dev/null &
-fi
-cd /var/www/html/OctoGUI
-res=$(git pull)
-check=$(echo "$res"|grep "up-to-date" -i)
-if [ -z "$check" ]
-then
+  cd /var/www/html/OctoGUI
+  res=$(git pull)
+  check=$(echo "$res"|grep "up-to-date" -i)
+  if [ -z "$check" ]
+  then
     sudo bash /var/www/html/OctoGUI/scripts/pi-setup.sh
-fi
+  fi
+else
+  if [ -f /home/pi/chromium.tar.gz -a ! -d /tmp/chromium ]
+  then
+    rm -rf /home/pi/.config/chromium
+    tar -xzf /home/pi/chromium.tar.gz -C /tmp
+    ln -nsf /tmp/chromium /home/pi/.config/chromium
+  fi
+  DISPLAY=:0.0 /usr/bin/xset s off
+  DISPLAY=:0.0 /usr/bin/xset -dpms
+  DISPLAY=:0.0 /usr/bin/xset s noblank
+  DISPLAY=:0.0 /usr/bin/unclutter -idle 0.1 &
+  if [ -f /home/pi/BURNIN ]
+  then
+    DISPLAY=:0.0 /usr/bin/chromium-browser --kiosk http://localhost/seeme/OctoGUI/www/burnin.html --fullscreen 2> /dev/null &
+  else
+    DISPLAY=:0.0 /usr/bin/chromium-browser --kiosk http://localhost/seeme/ --fullscreen 2> /dev/null &
+  fi
+  cd /var/www/html/OctoGUI
+  res=$(git pull)
+  check=$(echo "$res"|grep "up-to-date" -i)
+  if [ -z "$check" ]
+  then
+    sudo bash /var/www/html/OctoGUI/scripts/pi-setup.sh
+  fi
 
-if [ -f /home/pi/BURNIN ]
-then
-  cd /var/www/html/OctoGUI/scripts
-  for file in *gcode
-  do
-    if [ -f /home/pi/.octoprint/uploads/$file ]
-    then
-      check=$(diff $file /home/pi/.octoprint/uploads/$file)
-      if [ -n "$check" ]
+  if [ -f /home/pi/BURNIN ]
+  then
+    cd /var/www/html/OctoGUI/scripts
+    for file in *gcode
+    do
+      if [ -f /home/pi/.octoprint/uploads/$file ]
       then
-        cp -f $file /home/pi/.octoprint/uploads/$file
+        check=$(diff $file /home/pi/.octoprint/uploads/$file)
+        if [ -n "$check" ]
+        then
+          cp -f $file /home/pi/.octoprint/uploads/$file
+        fi
+      else
+        cp $file /home/pi/.octoprint/uploads/$file
       fi
-    else
-      cp $file /home/pi/.octoprint/uploads/$file
-    fi
-  done
-fi
+    done
+  fi
 
-sleep 15
-DISPLAY=:0.0 xdotool key ctrl+F5
+  sleep 15
+  DISPLAY=:0.0 xdotool key ctrl+F5
+
+fi
