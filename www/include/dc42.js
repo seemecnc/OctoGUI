730eb4203d8adfcd12ae962e3e213324ff815591
Ryan <ryan@seemecnc.com>
2017-01-24 12:03:41 -0500

    fixed v2 dc42

--- www/include/dc42.js
+++ www/include/dc42.js
@@ -53,6 +53,20 @@ function loadProbePoints(){
       bedRadius = 80;
       break;
 
+    case "rostock_max_v2":
+      probePoints.push([0,140]);
+      probePoints.push([121.24,70]);
+      probePoints.push([121.24,-70]);
+      probePoints.push([0,-140]);
+      probePoints.push([-121.24,-70]);
+      probePoints.push([-121.24,70]);
+      probePoints.push([0,70]);
+      probePoints.push([60.62,-35]);
+      probePoints.push([-60.62,-35]);
+      probePoints.push([0,0]);
+      bedRadius = 140;
+      break;
+
     case "rostock_max_v3":
       probePoints.push([0,140]);
       probePoints.push([121.24,70]);
