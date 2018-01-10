98b033ae165a2031c27c1ff743d41f20bf38afcb
Ryan <ryan@seemecnc.com>
2018-01-10 13:15:56 -0500

    added fsr flashing

---
+++ scripts/aflash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "RUNNING"
+if [ -n "$1" ]
+then
+  check=$(lsusb|grep '1781:0c9f')
+  if [ -n "$check" ]; then board=usbtiny;
+  else board=avrispmkII; fi
+else
+  echo "You must specify a file to flash"
+fi
+
+DISPLAY=:0.0 /usr/bin/lxterminal -e "avrdude -p t861 -c $board -U flash:w:$1" > /tmp/OK 2> /tmp/OK
+echo "DONE"
